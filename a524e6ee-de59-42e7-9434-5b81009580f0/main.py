f2b308040bb10942cc738f6e38789c516ff42543
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-02 09:15:32 -0400

    User Simone Pantaleoni saved changes to main.py at 06/02/2024, 13:15:31 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -29,7 +29,7 @@ class TradingStrategy(Strategy):
         # Calculate number of assets with positive momentum
         positive_momentum_assets = sum(m > 0 for m in momentum_scores.values())
         print(positive_momentum_assets)
-        positive_momentum_assets = 2
+        positive_momentum_assets = 3
 
         # Determine the allocation to crash protection asset
         if positive_momentum_assets <= 2:
