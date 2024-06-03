190cbe710777aee5bdc52f85e4be17e4c947327b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 15:17:18 -0400

    User Simone Pantaleoni saved changes to main.py at 06/03/2024, 19:17:17 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -56,7 +56,7 @@ class TradingStrategy(Strategy):
 
         momentum_scores = self.calculate_momentum_scores(data)
         ema = EMA("QQQ", datatick, self.STMA)[-1]
-        macd_signal = MACD("SPY", datatick, fast=15, slow=25)["signal"]  # Traditional MACD
+        macd_signal = MACD("SPY", datatick, fast=15, slow=25)[-1]]  # Traditional MACD
         mrktclose = datatick[-1]["QQQ"]["close"]
 
         # Calculate number of assets with positive momentum
