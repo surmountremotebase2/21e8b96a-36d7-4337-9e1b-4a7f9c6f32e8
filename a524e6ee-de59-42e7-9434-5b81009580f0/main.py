bc692c36dda578fe284b8dd6d78cc2f11691cc1e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 08:14:22 -0400

    User Simone Pantaleoni saved changes to main.py at 06/03/2024, 12:14:21 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -17,8 +17,8 @@ class TradingStrategy(Strategy):
         self.cplist = [self.crash_protection_asset1, self.crash_protection_asset2]
         self.RiskON = 3  #Number of Risk ON Assets
         self.RiskOFF = 2 #Number of Risk OFF Assets
-        self.LTMA = 100  #Long Term Moving Average
-        self.STMOM = 21   #Short Term Momentum
+        self.LTMA = 80  #Long Term Moving Average
+        self.STMOM = 15   #Short Term Momentum
         self.LTMOM = 128   #Short Term Momentum
 
     @property
