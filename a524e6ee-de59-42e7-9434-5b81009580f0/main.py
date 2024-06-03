43ba3335a7de0c0a2c54756a57383909e27a8f78
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 09:36:34 -0400

    User Simone Pantaleoni saved changes to main.py at 06/03/2024, 13:36:33 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -101,7 +101,7 @@ class TradingStrategy(Strategy):
             sma = self.calculate_sma(asset, datatick)
             if sma > 0:  # Avoid division by zero
                 #momentum_score = (close_data / sma) - 1
-                momentum_score = ( ((close_data / sma) *2) - (close_data / close_prices[-self.STMOM]) )
+                momentum_score = ( ((close_data / sma) *2) - (close_data / close_prices[-self.STMOM]) ) -1
             else:
                 momentum_score = 0
             momentum_scores[asset] = momentum_score
@@ -120,7 +120,7 @@ class TradingStrategy(Strategy):
             #close_prices = pd.DataFrame(close_prices)
             sma = self.calculate_sma(asset, data["ohlcv"])
             if sma > 0:  # Avoid division by zero
-                momentum_score = ( ((close_data / sma) *2) - ((close_data / close_prices[-self.STMOM])) )
+                momentum_score = ( ((close_data / sma) *2) - ((close_data / close_prices[-self.STMOM])) ) -1
                 #momentum_score = ( ((close_data / sma) ) )
             else:
                 momentum_score = 0
