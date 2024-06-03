c41b861f4c215e2b08e8e71d94cc6db8decd2b7d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 10:16:32 -0400

    User Simone Pantaleoni saved changes to main.py at 06/03/2024, 14:16:31 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -10,7 +10,7 @@ class TradingStrategy(Strategy):
         #self.tickers = ["SPY", "QQQ", "TECL", "IWM", "VGK", 
         self.tickers = ["SPY", "QQQ", "TECL", "IJT", 
                               #"EWJ", "EEM", "XLK", "HYG", "XLU", "XLV", "LQD",
-                              "XLK", "XLU", "XLV", "IEF", "SH", "FEZ", "TLT", "GLD",
+                              "XLK", "XLU", "XLV", "IEF", "FEZ", "TLT", "GLD",
                               "MTUM", "SOXX"]
         self.crash_protection_asset1 = "TIP"
         self.crash_protection_asset2 = "BIL"
