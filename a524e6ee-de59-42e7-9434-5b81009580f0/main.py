272527019b924150c0fa356ebeba346d9d5e93f0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-03 09:34:45 -0400

    User Simone Pantaleoni saved changes to main.py at 06/03/2024, 13:34:44 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -52,7 +52,8 @@ class TradingStrategy(Strategy):
             positive_momentum_assets = sum(m > 0 for m in momentum_scores.values())
             print(positive_momentum_assets)
             # Log the allocation for the current run.
-            log(f"NUM POS MOM {today.strftime('%Y-%m-%d')}: {positive_momentum_assets}")
+            log(f"Mom scores: {momentum_scores.values()}")
+            #log(f"NUM POS MOM {today.strftime('%Y-%m-%d')}: {positive_momentum_assets}")
             #positive_momentum_assets = 3
 
             # Determine the allocation to crash protection asset
