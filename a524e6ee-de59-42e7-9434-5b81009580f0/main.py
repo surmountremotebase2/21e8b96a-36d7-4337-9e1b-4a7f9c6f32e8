a7c6d1ac2924f85be6476f84af97a77efa55195f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-05 07:22:51 -0400

    User Simone Pantaleoni saved changes to multiple files files at 06/05/2024, 11:22:50 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -11,11 +11,11 @@ class TradingStrategy(Strategy):
         #self.tickers = ["SPY", "QQQ", "TECL", "IWM", "VGK", 
         self.tickers = ["SPY", "QQQ", "TECL", "DBC",
                               #"EWJ", "EEM", "XLK", "HYG", "XLU", "XLV", "LQD",
-                              "XLK", "XLV", "FEZ", "TLT", "IEF", "EWJ", "GLD",
+                              "XLK", "XLV", "XLU", "XLE", "FEZ", "TIP", "TLT", "IEF", "EWJ", "GLD",
                               "MTUM", "SOXX"]
         self.crash_protection_asset1 = "TIP"
         self.crash_protection_asset2 = "SHV"
-        self.SafeAssets = ["IEF", "TLT", "GLD"]
+        self.SafeAssets = ["IEF", "TLT", "GLD", "TIP", "DBC"]
         self.cplist = [self.crash_protection_asset1, self.crash_protection_asset2, "XLI", "XLU"]
         self.RiskON = 3  #Number of Risk ON Assets
         self.RiskOFF = 2 #Number of Risk OFF Assets
