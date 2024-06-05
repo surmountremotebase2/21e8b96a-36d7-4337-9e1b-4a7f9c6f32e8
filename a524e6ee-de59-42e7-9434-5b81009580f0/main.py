4aafee546349ab238def55d9e903bba3f80fe900
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-05 10:43:30 -0400

    User Simone Pantaleoni saved changes to main.py at 06/05/2024, 14:43:29 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -72,7 +72,7 @@ class TradingStrategy(Strategy):
         #log(f"NUM POS MOM {today.strftime('%Y-%m-%d')}: {positive_momentum_assets}")
         #positive_momentum_assets = 3
         # Determine allocations for assets with positive momentum
-        if mrktclose < mrktema and (mrktrsi > 70 or mrktrsi < 40:
+        if mrktclose < mrktema and (mrktrsi > 70 or mrktrsi < 40):
             del momentum_scores["TECL"]
             del momentum_scores["TQQQ"]
         
