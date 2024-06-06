f2570b1b5752a506fee565a5ba49eebb4b956de6
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-06 14:33:26 -0400

    User Simone Pantaleoni saved changes to main.py at 06/06/2024, 18:33:25 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -91,7 +91,7 @@ class TradingStrategy(Strategy):
 
         # Determine the allocation to crash protection asset
         #if (positive_momentum_assets <= 4 and TopMom in self.SafeAssets) and (xlu > xli and TopMom in self.SafeAssets):
-        if (positive_momentum_assets <= 4 and xlu > xli)
+        if (positive_momentum_assets <= 4 and xlu > xli):
             #log(f"RISK OFF: SHV")
             # Allocate everything to crash protection asset if 6 or fewer assets have positive momentum
             #cpmomentum_scores = self.calculate_cpmomentum_scores(data)
