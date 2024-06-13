fcc73e2c4e9167ed7750b8bce4cd0b19ca95a766
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-13 13:55:20 -0400

    User Simone Pantaleoni saved changes to main.py at 06/13/2024, 17:55:19 UTC time.

--- 5d05af69-cbc4-4b85-b046-5083ff02a123/main.py
+++ 5d05af69-cbc4-4b85-b046-5083ff02a123/main.py
@@ -162,7 +162,7 @@ class TradingStrategy(Strategy):
             ema = EMA(asset, datatick, 15)[-1]
             #ema = 0
             if sma > 0:  # Avoid division by zero
-                momentum_score = ( (((close_data / sma)) -1) *2 + ((close_data - close_prices[-self.STMOM]) / close_prices[-self.STMOM]) )
+                momentum_score = ( (((close_data / sma)) -1) + ((close_data - close_prices[-self.STMOM]) / close_prices[-self.STMOM]) )
                 #momentum_score = ( (close_data / sma) - 1 )
             else:
                 momentum_score = 0.0
