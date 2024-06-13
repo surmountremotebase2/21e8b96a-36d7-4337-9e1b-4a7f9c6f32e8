9a044dd2dc6643b6dde8cdc343637057f3a4bf61
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-13 11:26:22 -0400

    User Simone Pantaleoni saved changes to main.py at 06/13/2024, 15:26:21 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -20,7 +20,7 @@ class TradingStrategy(Strategy):
         #self.SafeAssets = ["IEF", "TLT", "GLD", "DBC", "UUP"]
         self.SafeAssets = ["IEF", "TLT", "GLD", "DBC", "UUP"]
         #self.CPAssets = ["IEF", "TLT", "SH", "GLD"]
-        self.CPAssets = ["IEF", "TLT", "GLD"]
+        self.CPAssets = ["IEF", "TLT", "SH", "GLD", "UUP"]
         self.cplist = [self.crash_protection_asset2, "XLI", "XLU"]
         self.RiskON = 2  #Number of Risk ON Assets
         self.RiskOFF = 2 #Number of Risk OFF Assets
