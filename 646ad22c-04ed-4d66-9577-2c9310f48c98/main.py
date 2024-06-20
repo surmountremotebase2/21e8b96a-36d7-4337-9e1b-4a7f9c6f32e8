77e90012fa626b036f8d64141ad02c70808d3616
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-20 14:53:49 -0400

    User Simone Pantaleoni saved changes to main.py at 06/20/2024, 18:53:48 UTC time.

--- 646ad22c-04ed-4d66-9577-2c9310f48c98/main.py
+++ 646ad22c-04ed-4d66-9577-2c9310f48c98/main.py
@@ -1,22 +1,22 @@
-
 from surmount.base_class import Strategy, TargetAllocation
 from surmount.technical_indicators import SMA
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.ticker = "TECL"
+        self.ticker = "SPY"
+        self.TEC = "TECL"
     
     @property
     def assets(self):
-        return [self.ticker]
+        return [self.ticker, self.TEC]
 
     @property
     def interval(self):
         return "1day"
     
     def run(self, data):
-        allocation_dict = {self.ticker: 0}  # Default to no allocation
+        allocation_dict = {self.TEC: 0}  # Default to no allocation
         ohlcv_data = data["ohlcv"]
         if len(ohlcv_data) < 26:
             log("Not enough data to evaluate 25-day high.")
@@ -25,8 +25,8 @@ class TradingStrategy(Strategy):
         # Extract closing prices
         closes = [day[self.ticker]["close"] for day in ohlcv_data]
         # Calculate the 25-day high
-        high_25_day = max(closes[-26:-2])
-        low_25_day = min(closes[-26:-2])
+        high_25_day = max(closes[-101:-2])
+        low_25_day = min(closes[-101:-2])
         
         # Current close price
         current_close = closes[-1]
@@ -34,9 +34,9 @@ class TradingStrategy(Strategy):
         # Check if current close is above 25-day high
         if current_close >= high_25_day:
             #log("Current close is above the 25-day high. Buying QQQ.")
-            allocation_dict[self.ticker] = 1  # Buy (allocate 100% to QQQ)
-        elif current_close < high_25_day:
+            allocation_dict[self.TEC] = 1  # Buy (allocate 100% to QQQ)
+        elif current_close < low_25_day:
             #log("Current close is below the 25-day high. Selling QQQ.")
-            allocation_dict[self.ticker] = 0  # Sell (close position)
+            allocation_dict[self.TEC] = 0  # Sell (close position)
 
         return TargetAllocation(allocation_dict)
