11f7e9824b961ac7a75d9e3570453390a4f50a94
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-30 09:13:01 -0400

    User Simone Pantaleoni saved changes to main.py at 06/30/2024, 13:13:00 UTC time.

--- 82fb653a-9fe3-453f-b5b3-55b7b6548c8d/main.py
+++ 82fb653a-9fe3-453f-b5b3-55b7b6548c8d/main.py
@@ -66,7 +66,7 @@ class TradingStrategy(Strategy):
             # Sell conditions based on SPY performance or holding duration
             if self.buy_signal:
                 ibs_today = self.IBS(today['close'], today['high'], today['low'])
-                if self.hold_days >= 10 or today['close'] > yesterday['high'] or ibs_today < .1:
+                if self.hold_days >= 10 or today['close'] > yesterday['high']:
                 #if self.hold_days >= 5 or today['close'] > yesterday['high']:
                     # Sell TQQQ (set allocation to 0) if holding period is 4 days or SPY closes higher than yesterday's high
                     self.buy_signal = False  # Reset buy signal
