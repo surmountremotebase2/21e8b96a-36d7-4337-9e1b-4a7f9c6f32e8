3b4fb0284e97dc23e761a75af43fd79825f6d5ca
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-02 10:42:55 -0400

    User Simone Pantaleoni saved changes to multiple files files at 07/02/2024, 14:42:54 UTC time.

--- fdcff9a4-14b6-4df2-822d-5c618440203f/main.py
+++ fdcff9a4-14b6-4df2-822d-5c618440203f/main.py
@@ -18,12 +18,13 @@ class TradingStrategy(Strategy):
         data = data["ohlcv"]
         
         # Calculate MACD with fast=5 and slow=15
-        macd_indicator = MACD("USO", data, 15, 25)
+        macd_indicator = MACD("USO", data, 15, 40)
         
         # Calculate 20 day SMA for "USO"
         #sma_20 = SMA("USO", data, 18)
-        sma_20 = EMA("USO", data, 18)
-        sma_100 = SMA("USO", data, 100)
+        sma_30 = EMA("USO", data, 30)
+        sma_10 = SMA("USO", data, 10)
+        sma_200 = SMA("USO", data, 200)
         
         if macd_indicator is None or sma_20 is None:
             return TargetAllocation({})
@@ -33,18 +34,20 @@ class TradingStrategy(Strategy):
         #log(f'Macd Cols: {MACDDFCols}')   #Macd Cols: Index(['MACD_5_15_9', 'MACDh_5_15_9', 'MACDs_5_15_9'], dtype='object')
         #log(f'Macd Indi: {macd_indicator}')
         current_price = data[-1]["USO"]["close"]
-        previous_macd = macd_indicator["MACD_15_25_9"][-2]
-        current_macd = macd_indicator["MACD_15_25_9"][-1]
-        MH = macd_indicator["MACDh_15_25_9"][-1]
-        macd_signal = macd_indicator["MACDs_15_25_9"][-1]
+        previous_macd = macd_indicator["MACD_15_40_9"][-2]
+        current_macd = macd_indicator["MACD_15_40_9"][-1]
+        MH = macd_indicator["MACDh_15_40_9"][-1]
+        macd_signal = macd_indicator["MACDs_15_40_9"][-1]
         #log(f' MACD: {current_macd} - MH: {MH} - Signal: {macd_signal}')
+        datadf = pd.DataFrame(data["USO"]["close"])
+        momentum = data["USO"]["close"].pct_change(10).iloc[-1]
 
         # MACD turning positive condition
-        if macd_signal > 0 and current_price > sma_20[-1] and current_price > sma_100[-1]:
+        if current_macd > 0 and current_price > sma_30[-1] and (sma_10[-1] > sma_200[-1] and sma_30[-1] > sma_200[-1]):
             #log("MACD turning positive, buying USO")
             uso_allocation = 1
         # Close position if "USO" close price crossed below the 20 days SMA or MACD turns negative
-        elif current_price < sma_20[-1] and macd_signal < 0:
+        elif current_price < sma_30[-1] and momentum < 0 and macd_signal < 0:
             #log("Closing USO position")
             uso_allocation = 0
 
