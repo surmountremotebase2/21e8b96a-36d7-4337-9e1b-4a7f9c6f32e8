f98d01eba39f562f6b665b063901df378ddef8bb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-02 10:02:37 -0400

    User Simone Pantaleoni saved changes to main.py at 07/02/2024, 14:02:36 UTC time.

--- 43d0a63f-761d-43f9-a44d-01d8011b7444/main.py
+++ 43d0a63f-761d-43f9-a44d-01d8011b7444/main.py
@@ -1,5 +1,5 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import RSI, EMA
+from surmount.technical_indicators import RSI, EMA, SMA
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
@@ -25,12 +25,12 @@ class TradingStrategy(Strategy):
         
         # Define the lookback periods for the technical indicators
         short_term_lookback = 10  # Short-term EMA
-        long_term_lookback = 100  # Long-term EMA
-        rsi_period = 30  # Relative Strength Index period
+        long_term_lookback = 50  # Long-term EMA
+        rsi_period = 10  # Relative Strength Index period
         
         # Calculate EMAs and RSI for USO
         short_term_ema = EMA(self.ticker, data["ohlcv"], short_term_lookback)
-        long_term_ema = EMA(self.ticker, data["ohlcv"], long_term_lookback)
+        long_term_ema = SMA(self.ticker, data["ohlcv"], long_term_lookback)
         rsi = RSI(self.ticker, data["ohlcv"], rsi_period)
         
         # Ensure we have enough data to consider a position
@@ -38,7 +38,7 @@ class TradingStrategy(Strategy):
             # Check if current short-term EMA is above long-term EMA, representing an uptrend
             if short_term_ema[-1] > long_term_ema[-1]:
                 # Additionally, check if RSI is indicating strong momentum but not overbought
-                if rsi[-1] > 50 and rsi[-1] < 70:
+                if rsi[-1] > 40 and rsi[-1] < 85:
                     # If both conditions are met, set allocation to 1 (i.e., 100% of portfolio)
                     uso_allocation = 1
                     
