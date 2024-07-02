30990446b5dfe12016136cdbfffcf298a5c03d76
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-02 14:27:41 -0400

    User Simone Pantaleoni saved changes to main.py at 07/02/2024, 18:27:40 UTC time.

--- fdcff9a4-14b6-4df2-822d-5c618440203f/main.py
+++ fdcff9a4-14b6-4df2-822d-5c618440203f/main.py
@@ -51,7 +51,7 @@ class TradingStrategy(Strategy):
             allocation_dict["USO"] = 0  # Close position
             allocation_dict["SCO"] = 1  # Close position
         # Check if the position should be closed - close is below the 15-day SMA or RSI < 50
-        elif current_price < bb['upper'][-1] or rsi[-1] < 52:
+        elif current_price < bb['upper'][-1] or rsi[-1] < 52 or current_price > sma15[-1]:
             allocation_dict["USO"] = 0  # Close position
             allocation_dict["SCO"] = 0  # Close position
         
