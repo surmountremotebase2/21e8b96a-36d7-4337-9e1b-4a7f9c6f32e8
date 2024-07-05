7717119b8b9d90ab854da1cd6fcf09e92d6fa663
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-05 12:32:46 -0400

    User Simone Pantaleoni saved changes to multiple files files at 07/05/2024, 16:32:45 UTC time.

--- fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
+++ fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
@@ -53,7 +53,7 @@ class TradingStrategy(Strategy):
         today = pd.to_datetime(today)
         dayweek = today.weekday()
 
-        dataDFQQQ = pd.DataFrame(datatick["QQQ"]["close"])
+        dataDFQQQ = pd.DataFrame(datatick[-253:]["QQQ"]["close"])
         
         
         #dataDF['date'] = pd.to_datetime(dataDF.loc['date'])
