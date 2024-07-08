f15245f69737ee8ed2a99b3cb5e77a6b778cff98
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-08 14:46:01 -0400

    User Simone Pantaleoni saved changes to main.py at 07/08/2024, 18:46:00 UTC time.

--- fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
+++ fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
@@ -11,8 +11,8 @@ class TradingStrategy(Strategy):
         # Define the global asset classes and the crash protection asset
         #self.tickers = ["QQQ", "XLV", "DBC", "TQQQ", "SPY", "TECL", "XLK", "SOXX", "IJT"]
         self.tickers = ["QQQ", "XLV", "DBC", "SPY", "TECL", "XLK", "SOXX", "IJT"]
-        self.SafeAssets = ["IEF", "TLT", "BIL"]
-        self.Canary = ["GLD", "SLV", "XLI", "XLU", "UUP", "DBB"]
+        self.SafeAssets = ["IEF", "TLT", "BIL", "GLD"]
+        self.Canary = ["SLV", "XLI", "XLU", "UUP", "DBB"]
         self.RiskAsset = "QQQ"
         self.SafeAsset = "BIL"
 
