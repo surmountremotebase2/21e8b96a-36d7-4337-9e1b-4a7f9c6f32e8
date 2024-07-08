b3c2872abb666170858344a46a3afbb4f42f21df
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-08 14:57:01 -0400

    User Simone Pantaleoni saved changes to main.py at 07/08/2024, 18:57:00 UTC time.

--- fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
+++ fa4e71fb-3308-488c-99b7-7637436aeff6/main.py
@@ -165,7 +165,7 @@ class TradingStrategy(Strategy):
             close_prices = [x[asset]['close'] for x in datatick[-self.VOLA_LOOKBACK:]]
             #close_prices = pd.DataFrame(close_prices)
             sma = self.calculate_sma(asset, data["ohlcv"])
-            ema = EMA(asset, datatick, 15)[-1]
+            ema = EMA(asset, datatick, 21)[-1]
             #ema = 0
             if sma > 0:  # Avoid division by zero
                 #momentum_score = ( (((close_data / sma)) -1) + ((close_data - close_prices[-self.STMOM]) / close_prices[-self.STMOM]) *2 )
