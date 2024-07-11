3c74a9b0930156007e557abf4a863bcc64a78c60
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-11 15:27:52 -0400

    User Simone Pantaleoni saved changes to main.py at 07/11/2024, 19:27:51 UTC time.

--- b03aee6c-ae27-41e0-93cd-b9eae05ff187/main.py
+++ b03aee6c-ae27-41e0-93cd-b9eae05ff187/main.py
@@ -37,7 +37,7 @@ class TradingStrategy(Strategy):
         if len(data) >= 2:
             current_close = data[-1]["QQQ"]["close"]
             tecl_close = data[-1]["TQQQ"]["close"]
-            previous_high = data[-2]["QQQ"]["high"]
+            previous_high = data[-3]["QQQ"]["high"]
             
             # RSI buy signal check
             if latest_rsi < 26:
