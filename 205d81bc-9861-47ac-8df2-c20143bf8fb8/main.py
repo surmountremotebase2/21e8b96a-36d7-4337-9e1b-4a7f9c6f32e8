c8e9e890c92a164b93d091675b22fb70695f64f5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-30 15:09:01 -0400

    User Simone Pantaleoni saved changes to main.py at 07/30/2024, 19:09:00 UTC time.

--- 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
+++ 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
@@ -25,9 +25,9 @@ class TradingStrategy(Strategy):
         #allocation = 0
         
         if len(d) >= 2:  # We need at least 2 days of data to proceed
-            closes = np.array([item["QQQ"]["close"] for item in d[-3:]])  # Last 3 days close prices [for current day and 2 days back]
-            highs = np.array([item["QQQ"]["high"] for item in d[-3:]])  # Last 3 days high prices
-            lows = np.array([item["QQQ"]["low"] for item in d[-3:]])  # Last 3 days low prices
+            closes = np.array([item["QQQ"]["close"] for item in d[-4:]])  # Last 3 days close prices [for current day and 2 days back]
+            highs = np.array([item["QQQ"]["high"] for item in d[-4:]])  # Last 3 days high prices
+            lows = np.array([item["QQQ"]["low"] for item in d[-4:]])  # Last 3 days low prices
 
             # Williams %R calculation for the previous day
             highest_high = np.max(highs[:-3])
