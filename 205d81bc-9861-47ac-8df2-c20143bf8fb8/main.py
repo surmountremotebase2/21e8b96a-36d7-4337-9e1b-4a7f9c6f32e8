fc23604b78c36b6298a473a23d6baf5b2d98d9b1
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-30 15:11:37 -0400

    User Simone Pantaleoni saved changes to main.py at 07/30/2024, 19:11:36 UTC time.

--- 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
+++ 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
@@ -30,8 +30,8 @@ class TradingStrategy(Strategy):
             lows = np.array([item["QQQ"]["low"] for item in d[-3:]])  # Last 3 days low prices
 
             # Williams %R calculation for the previous day
-            highest_high = np.max(highs[:-3])
-            lowest_low = np.min(lows[:-3])
+            highest_high = np.max(highs[:-2])
+            lowest_low = np.min(lows[:-2])
             close_yesterday = closes[-2]
             williams_r_yesterday = ((highest_high - close_yesterday) / (highest_high - lowest_low)) * -100
 
