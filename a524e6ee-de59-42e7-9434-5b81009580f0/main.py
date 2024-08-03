e247c3fec3f0653d75c251d3266fe5f8153493eb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-03 08:36:57 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/03/2024, 15:36:56 UTC time.

--- a524e6ee-de59-42e7-9434-5b81009580f0/main.py
+++ a524e6ee-de59-42e7-9434-5b81009580f0/main.py
@@ -89,7 +89,7 @@ class TradingStrategy(Strategy):
         # Calculate number of assets with positive momentum
         positive_momentum_assets = sum(m > 0 for m in momentum_scores.values())
 
-        sorted_assets_by_momentum = sorted(momentum_scores, key=momentum_scores.get, reverse=True)[:self.RiskON]
+        sorted_assets_by_momentum = sorted(momentum_scores, key=momentum_scores.get, reverse=True)
         #sorted_assets_by_momentum = sorted(momentum_scores, key=momentum_scores.get, reverse=True)[:5]
         TopMom = sorted_assets_by_momentum[0]
         #log(f"TopMom: {TopMom}")
