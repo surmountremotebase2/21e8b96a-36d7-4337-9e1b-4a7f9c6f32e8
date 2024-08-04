fe39019017fe5c22f281b3d6d7c84e2c363db1ac
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-04 07:06:31 -0700

    User Simone Pantaleoni saved changes to main.py at 08/04/2024, 14:06:31 UTC time.

--- 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
+++ 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
@@ -54,7 +54,7 @@ class TradingStrategy(Strategy):
             #log(f"Williams %R Yesterday: {williams_r_yesterday}, Williams %R Today: {williams_r_today}")
 
             # Buy signal based on Williams %R and conditions for selling
-            if williams_r_yesterday < -98:
+            if williams_r_yesterday < -98 and williams_r_yesterday > -130:
                 allocation["TQQQ"] = 1
                 allocation["BIL"] = 0
                 self.bull = True
