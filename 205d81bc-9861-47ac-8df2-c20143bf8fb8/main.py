8897f9e4d51295b612589f23b4da48da79dc8c33
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-04 06:49:54 -0700

    User Simone Pantaleoni saved changes to main.py at 08/04/2024, 13:49:54 UTC time.

--- 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
+++ 205d81bc-9861-47ac-8df2-c20143bf8fb8/main.py
@@ -59,7 +59,7 @@ class TradingStrategy(Strategy):
                 allocation["BIL"] = 0
                 self.bull = True
                 self.allocation = allocation
-            if close_today > highs[-2] or williams_r_today > -35:  # Exit conditions
+            if close_today > highs[-2] or williams_r_today > -40:  # Exit conditions
                 allocation["TQQQ"] = 0
                 allocation["BIL"] = 1
                 self.bull = False
