235b81e2efb6fba51836111e1b9b137ffbd2e0a4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-12 06:41:41 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/12/2024, 13:41:40 UTC time.

--- 43d0a63f-761d-43f9-a44d-01d8011b7444/main.py
+++ 43d0a63f-761d-43f9-a44d-01d8011b7444/main.py
@@ -5,21 +5,72 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
     def __init__(self):
         # Only trading with "USO" ETF
-        self.ticker = "SPY"
+        self.ticker = ["SPY", "QQQ", "BIL"]
+        self.RiskOn = "QQQ"
+        self.RiskOff = "BIL"
 
     @property
     def assets(self):
         # Defines the asset to trade
-        return [self.ticker]
+        return self.ticker
 
     @property
     def interval(self):
         # Sets the strategy to run on daily data
         return "1day"
 
-    def run(self, data):
-        # The trading logic goes here
-        uso_allocation = 1.0
 
-        # Return the allocation object with the calculated allocation
-        return TargetAllocation({self.ticker: uso_allocation})
+    def realized_volatility_daily(self, series_log_return):
+        """
+        Get the daily realized volatility which is calculated as the square root
+        of sum of squares of log returns within a specific window interval 
+        """
+        n = len(series_log_return)
+        vola =  np.sqrt(np.sum(series_log_return**2)/(n - 1))
+        return vola
+
+
+
+    def run(self, data):
+        self.count -= 1
+        spy_data = [entry['QQQ']['close'] for entry in data['ohlcv'] if 'SPY' in entry]
+        spy_dates = [entry['QQQ']['date'] for entry in data['ohlcv'] if 'SPY' in entry]
+        spy_data = pd.DataFrame(spy_data, columns=['close'])
+        spy_data['returns'] = 100 * spy_data.close.pct_change().dropna()
+        # CALCULATE LOG RETURNS BASED ON ABOVE FORMULA
+        spy_data['log_returns'] = np.log(spy_data.close/spy_data.close.shift(1))
+        spy_data = spy_data.fillna(0)
+        INTERVAL_WINDOW = 60
+        n_future = 20
+
+
+        if len(spy_data) > n_future:
+            # GET BACKWARD LOOKING REALIZED VOLATILITY
+            spy_data['vol_current'] = spy_data.log_returns.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily)
+            spy_data['vol_current'] = spy_data['vol_current'].bfill()
+            # GET FORWARD LOOKING REALIZED VOLATILITY 
+            spy_data['vol_future'] = spy_data.log_returns.shift(n_future).fillna(0).rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily)
+            spy_data['vol_future'] = spy_data['vol_future'].bfill()
+            volaT = np.percentile(spy_data['vol_current'], 50)
+            volaH = np.percentile(spy_data['vol_current'], 80)
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
+
+                # Check if the current ATR or Realized Volatility is above the 7th or 8th decile
+            if (spy_data['vol_current'].iloc[-1] > spy_data['vol_future'].iloc[-1] and spy_data['vol_current'].iloc[-1] > volaT):
+
+                allocation_dict = {self.RiskOn: 0; self.RiskOff: 1.0}
+                if spy_data['vol_current'].iloc[-1] > volaH:
+                    self.count = 20
+                else:
+                    self.count = 15
+            elif self.count < 1:
+
+                allocation_dict = {self.RiskOn: 1.0; self.RiskOff: 0}
+
+            else:
+                allocation_dict = {self.RiskOn: 0; self.RiskOff: 1.0}
+            
+            return TargetAllocation(allocation_dict)
+        else:
+            allocation_dict = {self.RiskOn: 0; self.RiskOff: 1.0}
+            return TargetAllocation(allocation_dict)
