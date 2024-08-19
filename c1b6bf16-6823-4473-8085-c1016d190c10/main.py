5e81b1eeab9cf230fd0400c1f6f8bc44db8e3b93
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-19 05:52:57 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/19/2024, 12:52:56 UTC time.

--- c1b6bf16-6823-4473-8085-c1016d190c10/main.py
+++ c1b6bf16-6823-4473-8085-c1016d190c10/main.py
@@ -50,16 +50,18 @@ class TradingStrategy(Strategy):
         # Determine if it's time to trade TMV or TLT based on the calendar day
         if today_date == month_end:
             allocation[self.ShortBond] = .3  # Buy TMV at month's end
-            allocation[self.Gold] = .7
-            allocation[self.Equity] = 0
+            allocation[self.LongBond] = 0
+            allocation[self.Gold] = .3
+            allocation[self.Equity] = .4
         elif today_date.day == 5 and last_trading_day_tmv >= today_date:
             allocation[self.ShortBond] = 0  # Sell TMV at the close of the new month's seventh day
             allocation[self.LongBond] = 0
             allocation[self.Gold] = 0
+            allocation[self.Equity] = 0
             #allocation[self.Equity] = 1
         elif today_date.day == 8 and last_trading_day_tlt >= today_date:
             allocation[self.LongBond] = 1  # Buy TLT on the eighth day of the new month
             allocation[self.Gold] = 0
-            allocation[self.Equity] = 0
+
         
         return TargetAllocation(allocation)
