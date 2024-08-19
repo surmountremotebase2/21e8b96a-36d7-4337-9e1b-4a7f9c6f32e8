f740b893f1e91d6c4933bb29137ec8bb3a74b814
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-19 12:11:16 -0700

    User Simone Pantaleoni saved changes to main.py at 08/19/2024, 19:11:15 UTC time.

--- c1b6bf16-6823-4473-8085-c1016d190c10/main.py
+++ c1b6bf16-6823-4473-8085-c1016d190c10/main.py
@@ -100,17 +100,17 @@ class TradingStrategy(Strategy):
             
             else:
                 if today_date == month_end:
-                    allocation_dict[self.ShortBond] = 1  # Buy TMV at month's end
+                    allocation_dict[self.ShortBond] = .5  # Buy TMV at month's end
                     allocation_dict[self.LongBond] = 0
-                    allocation_dict[self.Equity] = 0
+                    allocation_dict[self.Equity] = .5
                 elif today_date.day == 7 and last_trading_day_tmv >= today_date:
                     allocation_dict[self.ShortBond] = 0  # Sell TMV at the close of the new month's seventh day
                     allocation_dict[self.LongBond] = 0
                     allocation_dict[self.Equity] = 0
                 elif today_date.day == 8 and last_trading_day_tlt >= today_date:
-                    allocation_dict[self.LongBond] = 1  # Buy TLT on the eighth day of the new month
+                    allocation_dict[self.LongBond] = .5  # Buy TLT on the eighth day of the new month
                     allocation_dict[self.ShortBond] = 0
-                    allocation_dict[self.Equity] = 0
+                    allocation_dict[self.Equity] = .5
 
             
             return TargetAllocation(allocation_dict)
