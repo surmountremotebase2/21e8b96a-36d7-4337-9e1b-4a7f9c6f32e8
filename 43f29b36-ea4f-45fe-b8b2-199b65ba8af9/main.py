86e95c037677fa7cc533195ed1699b705ec2cdcc
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 10:33:44 -0700

    User Simone Pantaleoni saved changes to main.py at 08/26/2024, 17:33:43 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -60,7 +60,7 @@ class TradingStrategy(Strategy):
         ratioMAL = ratioDF["ratio"].rolling(LongMA).mean().fillna(0)
         spyLongMA = LongMA
         mrktMAS = EMA(self.mrkt, data["ohlcv"], 3)
-        mrktMAL = EMA(self.mrkt, data["ohlcv"], spyLongMA)
+        mrktMAL = EMA(self.mrkt, data["ohlcv"], 200)
 
         # Check if the current 20-day SMA and the lower Bollinger band are above the 100-day SMA, indicating a buy signal
         if ratioMAS.iloc[-1] > ratioMAL.iloc[-1] and mrktMAS[-1] > mrktMAL[-1]:
