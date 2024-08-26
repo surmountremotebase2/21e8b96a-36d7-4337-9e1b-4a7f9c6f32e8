bb28b8d65ca2faf2d243b6c1969ada5d733d22a1
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 07:36:59 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/26/2024, 14:36:58 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -47,8 +47,8 @@ class TradingStrategy(Strategy):
         gld_prices = [x["GLD"]["close"] for x in data["ohlcv"]]
         spyDF = pd.DataFrame(spy_prices, columns=["close"])
         spy_ret = np.log(spyDF.close/spyDF.close.shift(1))
-        spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 50
-        LongMA = int(126 * (1 - spyvola.iloc[-1]))
+        spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 80
+        LongMA = int(82 * (1 - spyvola.iloc[-1]))
         if LongMA <= 0:
             LongMA = 5
         ratio = [spy/gld for spy, gld in zip(spy_prices, gld_prices)]
@@ -56,8 +56,7 @@ class TradingStrategy(Strategy):
         # Calculate moving averages and Bollinger Bands for the ratio
         # ratioT = {"ratio": {"close": ratio}}
         ratioDF = pd.DataFrame(ratio, columns=["ratio"])
-        #ratioMAS = ratioDF["ratio"].rolling(3).mean().fillna(0)
-        ratioMAS = ratioDF["ratio"]
+        ratioMAS = ratioDF["ratio"].rolling(3).mean().fillna(0)
         ratioMAL = ratioDF["ratio"].rolling(LongMA).mean().fillna(0)
 
         # Check if the current 20-day SMA and the lower Bollinger band are above the 100-day SMA, indicating a buy signal
