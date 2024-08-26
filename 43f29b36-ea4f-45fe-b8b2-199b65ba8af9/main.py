3106a518fe5c6c43f8cf5d998cf886b898e0aee8
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 07:50:25 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/26/2024, 14:50:24 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -50,7 +50,7 @@ class TradingStrategy(Strategy):
         spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 100
         LongMA = int(82 * (1 - spyvola.iloc[-1]))
         if LongMA <= 5:
-            LongMA = int(spyvola.iloc[-1] * 10)
+            LongMA = int(spyvola.iloc[-1] * INTERVAL_WINDOW)
         ratio = [spy/gld for spy, gld in zip(spy_prices, gld_prices)]
 
         # Calculate moving averages and Bollinger Bands for the ratio
