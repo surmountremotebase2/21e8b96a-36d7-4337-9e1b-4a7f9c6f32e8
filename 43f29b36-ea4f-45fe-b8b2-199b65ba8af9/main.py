daab659c8e08d4b97fb68392d50c9485a0d57b4e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 07:39:44 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/26/2024, 14:39:43 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -47,10 +47,10 @@ class TradingStrategy(Strategy):
         gld_prices = [x["GLD"]["close"] for x in data["ohlcv"]]
         spyDF = pd.DataFrame(spy_prices, columns=["close"])
         spy_ret = np.log(spyDF.close/spyDF.close.shift(1))
-        spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 80
+        spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 100
         LongMA = int(82 * (1 - spyvola.iloc[-1]))
         if LongMA <= 0:
-            LongMA = 5
+            LongMA = 82
         ratio = [spy/gld for spy, gld in zip(spy_prices, gld_prices)]
 
         # Calculate moving averages and Bollinger Bands for the ratio
