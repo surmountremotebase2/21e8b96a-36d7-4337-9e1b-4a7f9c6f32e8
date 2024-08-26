5118a677e17bbe8ef172f75fb734a853b81026d4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 09:29:52 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/26/2024, 16:29:51 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -49,7 +49,7 @@ class TradingStrategy(Strategy):
         spy_ret = np.log(spyDF.close/spyDF.close.shift(1))
         spyvola = spy_ret.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily) * 100
         LongMA = int(126 * (1 - spyvola.iloc[-1]))
-        if LongMA <= 15:
+        if LongMA <= 5:
             LongMA = int(spyvola.iloc[-1] * INTERVAL_WINDOW)
         ratio = [spy/gld for spy, gld in zip(spy_prices, gld_prices)]
 
