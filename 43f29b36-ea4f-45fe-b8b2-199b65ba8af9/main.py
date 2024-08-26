4e21880f2379ddb669214ce4ae5a993230deb3bd
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-26 06:30:36 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/26/2024, 13:30:35 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -23,9 +23,19 @@ class TradingStrategy(Strategy):
         # QQQ is the asset that will be traded in this strategy
         return self.tickers
 
+    def realized_volatility_daily(self, series_log_return):
+        """
+        Get the daily realized volatility which is calculated as the square root
+        of sum of squares of log returns within a specific window interval 
+        """
+        n = len(series_log_return)
+        vola =  np.sqrt(np.sum(series_log_return**2)/(n - 1))
+        return vola
+
     def run(self, data):
         # Initialize QQQ stake to 0, meaning no position by default
         qqq_stake = 1
+        INTERVAL_WINDOW = 126
 
         # Ensure there's enough data for BTCUSD, GLD, and QQQ to generate signals
         if len(data["ohlcv"]) < 100:
@@ -35,13 +45,15 @@ class TradingStrategy(Strategy):
         # Calculate the ratio of BTCUSD to GLD
         spy_prices = [x["SPY"]["close"] for x in data["ohlcv"]]
         gld_prices = [x["GLD"]["close"] for x in data["ohlcv"]]
+        spyvola = spy_prices.log_returns.rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily)
+        LongMA = int(INTERVAL_WINDOW * (1 - spyvola))
         ratio = [spy/gld for spy, gld in zip(spy_prices, gld_prices)]
 
         # Calculate moving averages and Bollinger Bands for the ratio
         # ratioT = {"ratio": {"close": ratio}}
         ratioDF = pd.DataFrame(ratio, columns=["ratio"])
         ratioMAS = ratioDF["ratio"].rolling(5).mean().fillna(0)
-        ratioMAL = ratioDF["ratio"].rolling(120).mean().fillna(0)
+        ratioMAL = ratioDF["ratio"].rolling(LongMA).mean().fillna(0)
 
         # Check if the current 20-day SMA and the lower Bollinger band are above the 100-day SMA, indicating a buy signal
         if ratioMAS.iloc[-1] > ratioMAL.iloc[-1]:
