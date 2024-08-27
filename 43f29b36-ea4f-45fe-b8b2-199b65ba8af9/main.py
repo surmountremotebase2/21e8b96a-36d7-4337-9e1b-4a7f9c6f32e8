82623e187630a2ea6df240bfa849311e44c06d3c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-08-27 05:27:19 -0700

    User Simone Pantaleoni saved changes to multiple files files at 08/27/2024, 12:27:18 UTC time.

--- 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
+++ 43f29b36-ea4f-45fe-b8b2-199b65ba8af9/main.py
@@ -70,7 +70,7 @@ class TradingStrategy(Strategy):
 
         # Check if the current 20-day SMA and the lower Bollinger band are above the 100-day SMA, indicating a buy signal
         #if ratioMAS.iloc[-1] > ratioMAL.iloc[-1] and mrktMAS[-1] > mrktMAL[-1]:
-        if ratioMAS.iloc[-1] > ratioMAL.iloc[-1] and spydm.iloc[-1] > glddm.iloc[-1]:
+        if ratioMAS.iloc[-1] > ratioMAL.iloc[-1] and spydm > glddm:
             #log("Buy signal detected.")
             #log(f"spyvola: {spyvola.iloc[-1]}  -- LongMA: {LongMA}")
             qqq_stake = 1  # Allocating 100% to QQQ based on the buy signal
