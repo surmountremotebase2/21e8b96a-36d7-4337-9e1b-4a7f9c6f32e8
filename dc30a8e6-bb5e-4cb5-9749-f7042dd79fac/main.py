0c599c45ab78f08207ccc821b99663a681fca32c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-08 09:19:55 -0700

    User Simone Pantaleoni saved changes to multiple files files at 09/08/2024, 16:19:54 UTC time.

--- dc30a8e6-bb5e-4cb5-9749-f7042dd79fac/main.py
+++ dc30a8e6-bb5e-4cb5-9749-f7042dd79fac/main.py
@@ -49,6 +49,8 @@ class TradingStrategy(Strategy):
             #log("OFF #2")
             self.trade = 0
             self.count = 10
+
+            
         elif spy_ema7[-1] > lower_band and spy_ema7[-3] <= bb["lower"][-3] and (spy_rsi[-3] < 50) and self.count < 1:
             self.trade = 1
         elif spy_ema7[-1] >= upper_band and spy_ema7[-2] >= bb["upper"][-2] and spy_rsi[-1] >= 65 and mrktSlope[-1] > 0 and self.count < 1:
