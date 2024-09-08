65a01abeb567510c7f236524b0c44c9d985b7d64
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-08 06:13:54 -0700

    User Simone Pantaleoni saved changes to main.py at 09/08/2024, 13:13:53 UTC time.

--- dc30a8e6-bb5e-4cb5-9749-f7042dd79fac/main.py
+++ dc30a8e6-bb5e-4cb5-9749-f7042dd79fac/main.py
@@ -4,8 +4,8 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["SPY", "IWS", "QQQ", "TQQQ"]
-        self.mrkt = "IWS"
+        self.tickers = ["SPY", "TQQQ", "QQQ", "TQQQ"]
+        self.mrkt = "TQQQ"
         self.tradeAsset = "TQQQ"
         self.std_dev_multiplier = .6
         self.trade = 0
