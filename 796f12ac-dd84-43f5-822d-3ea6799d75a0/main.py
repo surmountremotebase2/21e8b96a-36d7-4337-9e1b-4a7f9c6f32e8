a59317480575b28a2dc30e3f4016e11a956b3a3d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-03-25 00:40:58 +0500

    User Simone Pantaleoni saved changes to main.py at 03/24/2025, 19:40:57 UTC time.

--- 796f12ac-dd84-43f5-822d-3ea6799d75a0/main.py
+++ 796f12ac-dd84-43f5-822d-3ea6799d75a0/main.py
@@ -4,7 +4,7 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["TSM", "BABA", "TCEHY", "SE", "MELI", "RELI"]
+        self.tickers = ["TSM", "BABA", "TCEHY", "SE", "MELI", "AMX", "PBR"]
 
     @property
     def interval(self):
