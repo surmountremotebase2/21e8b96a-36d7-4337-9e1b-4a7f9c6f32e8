2b57f1d42b86f3ffef3ab70e7943711eef52802b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-03-26 23:44:48 +0500

    User Simone Pantaleoni saved changes to main.py at 03/26/2025, 18:44:47 UTC time.

--- 57ce1f95-49b5-47d9-aa83-671eede8352e/main.py
+++ 57ce1f95-49b5-47d9-aa83-671eede8352e/main.py
@@ -38,8 +38,8 @@ class TradingStrategy(Strategy):
         """
         allocations = {ticker: 1 / len(self.assets) for ticker in self.assets}  # Default equal allocation
         ohlcv = data["ohlcv"]
-        inflation_data = data.get('5year_forward_inflation_expected_rate')
-        cpi = self.data_list.iloc[0][('5year_forward_inflation_expected_rate')]
+        inflation_data = data[("5year_forward_inflation_expected_rate",)]
+
 
         if len(ohlcv) < 100:
             return TargetAllocation(allocations)
@@ -58,7 +58,7 @@ class TradingStrategy(Strategy):
         if gld_prices[0] and gld_prices[-1] and ((gld_prices[-1] - gld_prices[0]) / gld_prices[0]) > 0.15:
             allocations["GLD"] -= 0.10  # Reduce allocation to GLD
             log("GLD up more than 15% this quarter, reducing allocation")
-            log(f"{cpi}")
+            log(f"{inflation_data}")
 
         # Stop-Loss Rule: If oil stocks drop >10% in a month, trim allocation
         for ticker in ["XOM", "COP"]:
