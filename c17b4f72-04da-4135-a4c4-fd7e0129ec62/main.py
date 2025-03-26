1ae879c1d019ddf01e748271c17db3d945b73300
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-03-26 23:43:31 +0500

    User Simone Pantaleoni saved changes to main.py at 03/26/2025, 18:43:30 UTC time.

--- c17b4f72-04da-4135-a4c4-fd7e0129ec62/main.py
+++ c17b4f72-04da-4135-a4c4-fd7e0129ec62/main.py
@@ -1,1 +1,39 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.data import CivilianUnemployment
+from surmount.logging import log
+
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["AAPL"]
+        self.data_list = [CivilianUnemployment()]
+
+    @property
+    def interval(self):
+        # Use daily data as economic indicators do not update more frequently
+        return "1day"
+
+    @property
+    def assets(self):
+        # We are only trading AAPL based on economic indicators
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        allocation_dict = {"AAPL": 0} 
+        unemployment_data = data[("civilian_unemployment",)]
+
+        if  len(unemployment_data) > 1:
+            latest_unemployment_change = unemployment_data[-1]["value"] - unemployment_data[-2]["value"]
+
+            if latest_unemployment_change <= 0:
+                allocation_dict["AAPL"] = 1  # Full allocation to AAPL
+            elif latest_unemployment_change > 0:
+                allocation_dict["AAPL"] = 0  # No position in AAPL
+                
+            log(f"Latest Unemployment Change: {latest_unemployment_change}")
+
+        return TargetAllocation(allocation_dict)
