ba506576f496db4461fad8b3aa8518634812847a
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-03-28 01:49:04 +0500

    User Simone Pantaleoni saved changes to main.py at 03/27/2025, 20:49:03 UTC time.

--- 898808f2-0084-45a2-8563-fba7d12fdedc/main.py
+++ 898808f2-0084-45a2-8563-fba7d12fdedc/main.py
@@ -4,10 +4,10 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["COIN", "SQ", "NVDA", "MSTR", "AMD", "BITO"]
+        self.tickers = ["COIN", "NVDA", "MSTR", "AMD", "BITO"]
         self.btc_ticker = "BTC-USD"
         self.data_list = []
-        self.weights = {"COIN": 0.1, "SQ": 0.1, "MSTR": 0.1, "NVDA": 0.1, "AMD": 0.1, "BITO": 0.1}
+        self.weights = {"COIN": 0.1, "MSTR": 0.1, "NVDA": 0.1, "AMD": 0.1, "BITO": 0.1}
 
     @property
     def interval(self):
