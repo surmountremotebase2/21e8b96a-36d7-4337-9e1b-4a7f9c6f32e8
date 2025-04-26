2ae33062f7349110fd4d24dcc09198352e08f7b9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-04-26 08:30:10 -0700

    User Simone Pantaleoni saved changes to multiple files files at 04/26/2025, 15:30:09 UTC time.

--- 8c9af028-c923-400b-96ff-ad0253d9ce37/main.py
+++ 8c9af028-c923-400b-96ff-ad0253d9ce37/main.py
@@ -6,7 +6,7 @@ class TradingStrategy(Strategy):
 
    def __init__(self):
       self.tickers = ["NVDA", "GOOGL", "MSFT", "AMZN", "TSLA", "IBM", "INTC", "BIDU", "AMD", "CRM", "NOW", "TWLO", "PATH", "CGNX", "MU", "ASML", "DOCU", "CRWD", "OKTA", "PLTR", "ZS", "FSLY", "SNOW", "DDOG", "SNPS", "CDNS", "ANSS", "ADSK", "NTNX", "APPN", "ANET", "TDC", "PEGA", "VRNS", "AI", "ESTC", "TENB"]
-      self.weights = [0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.04, 0.03, 0.04, 0.02, 0.02, 0.02, 0.03, 0.02, 0.02, 0.04, 0.02, 0.03, 0.02, 0.01, 0.01, 0.02, 0.02, 0.03, 0.01, 0.01, 0.01, 0.01, 0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.06]
+      self.weights = [0.06, 0.06, 0.06, 0.06, 0.06, 0.04, 0.04, 0.04, 0.04, 0.03, 0.03, 0.02, 0.02, 0.02, 0.04, 0.02, 0.03, 0.02, 0.01, 0.01, 0.02, 0.02, 0.03, 0.01, 0.01, 0.01, 0.01, 0.02, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.06]
       self.equal_weighting = False
       self.count = 0
 
