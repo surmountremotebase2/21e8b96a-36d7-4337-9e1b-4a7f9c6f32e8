266f8ae2a3386e2bac0bd8bc5f030a86b53aea4c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-04-26 08:53:30 -0700

    User Simone Pantaleoni saved changes to multiple files files at 04/26/2025, 15:53:29 UTC time.

--- 8c9af028-c923-400b-96ff-ad0253d9ce37/main.py
+++ 8c9af028-c923-400b-96ff-ad0253d9ce37/main.py
@@ -69,7 +69,7 @@ class TradingStrategy(Strategy):
             spy_data['vol_future'] = spy_data.log_returns.shift(n_future).fillna(0).rolling(window=INTERVAL_WINDOW).apply(self.realized_volatility_daily)
             spy_data['vol_future'] = spy_data['vol_future'].bfill()
 
-            volaT = np.percentile(spy_data['vol_current'], 50)  # Increased threshold
+            volaT = np.percentile(spy_data['vol_current'], 65)  # Increased threshold
             volaH = np.percentile(spy_data['vol_current'], 95)  # Increased threshold
 
             allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))}
