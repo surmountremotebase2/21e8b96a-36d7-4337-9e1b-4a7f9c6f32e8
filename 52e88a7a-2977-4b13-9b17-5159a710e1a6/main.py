55f6dd9b0f5d0bf06e58685e095f21f67a9a539f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-06-03 00:12:19 +0500

    User Simone Pantaleoni saved changes to main.py at 06/02/2025, 19:12:18 UTC time.

--- 52e88a7a-2977-4b13-9b17-5159a710e1a6/main.py
+++ 52e88a7a-2977-4b13-9b17-5159a710e1a6/main.py
@@ -5,7 +5,7 @@ from surmount.logging import log
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["SMR", "BWXT", "LEU", "CEG", "VST", "OKLO", "CCJ"]
+        self.tickers = ["SMR", "BWXT", "LEU", "CEG", "VST", "OKLO", "CCJ", "URA"]
 
     @property
     def interval(self):
@@ -23,9 +23,10 @@ class TradingStrategy(Strategy):
         ohlcv = data["ohlcv"]
         allocations = {}
         weights = {ticker: 1 for ticker in self.tickers}
+        index = ohlcv["URA"]["close"]
 
         # Edge case: Not enough data
-        if len(ohlcv["CCJ"]["close"]) < 60:
+        if len(index) < 60:
             log("Insufficient data. Using equal weights.")
             equal_weight = 1.0 / len(self.tickers)
             return TargetAllocation({ticker: equal_weight for ticker in self.tickers})
